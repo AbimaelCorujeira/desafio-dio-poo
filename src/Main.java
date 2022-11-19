00db2f2e5ff67ad072d056ea7f1b2bda3b4b65f5
AbimaelCorujeira <corujeira.abimael@gmail.com>
2022-11-19 15:06:27 -0300

    Final

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-import dev.corujeira.abimael.dominio.Conteudo;
-import dev.corujeira.abimael.dominio.Curso;
-import dev.corujeira.abimael.dominio.Mentoria;
+import dev.corujeira.abimael.dominio.*;
 
 import java.time.LocalDate;
 
@@ -23,8 +21,39 @@ public class Main {
         mentoria.setDescricao("Descrição da mentoria Java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria);
+//        System.out.println(curso1);
+//        System.out.println(curso2);
+//        System.out.println(mentoria);
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devAbimael = new Dev();
+        devAbimael.setNome("Abimael");
+        devAbimael.inscreverBootcamp(bootcamp);
+        System.out.println("\nConteúdos Inscritos Abimael: " + devAbimael.getConteudosInscritos());
+        devAbimael.progredir();
+        devAbimael.progredir();
+        System.out.println("\nConteúdos Inscritos Abimael: " + devAbimael.getConteudosInscritos());
+        System.out.println("Conteúdos Concluidos Abimael: " + devAbimael.getConteudosConcluidos());
+        System.out.println("XP: " + devAbimael.calcularXp());
+
+        Dev devMarcela = new Dev();
+        devMarcela.setNome("Marcela");
+        devMarcela.inscreverBootcamp(bootcamp);
+        System.out.println("\nConteúdos Inscritos Marcela: " + devMarcela.getConteudosInscritos());
+        devMarcela.progredir();
+        devMarcela.progredir();
+        devMarcela.progredir();
+        System.out.println("\nConteúdos Inscritos Marcela: " + devMarcela.getConteudosInscritos());
+        System.out.println("Conteúdos Concluidos Marcela: " + devAbimael.getConteudosConcluidos());
+        System.out.println("XP: " + devMarcela.calcularXp());
+
+
+
     }
 }
